ef20b8524572f841a336ea6ec1299f2959d59dbf
beigirad <f.beigirad@gmail.com>
2019-03-01 02:21:49 +0330

    added room typeConvertor

--- data/src/main/java/ir/beigirad/cache/db/AppDatabase.kt
+++ data/src/main/java/ir/beigirad/cache/db/AppDatabase.kt
@@ -2,10 +2,13 @@ package ir.beigirad.cache.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import ir.beigirad.cache.convertor.PairConverter
 import ir.beigirad.cache.dao.VenueDao
 import ir.beigirad.cache.model.VenueModel
 
 @Database(entities = [VenueModel::class], version = 1, exportSchema = false)
+@TypeConverters(value = [PairConverter::class])
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun venueDao(): VenueDao
