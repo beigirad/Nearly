1f7a7661dc10dfb1e38f2c8668f5cc6cc450343c
beigirad <f.beigirad@gmail.com>
2019-03-01 14:05:16 +0330

    added MainActivity

---
+++ app/src/main/java/ir/beigirad/nearly/MainActivity.kt
@@ -0,0 +1,9 @@
+package ir.beigirad.nearly
+
+import ir.beigirad.zeroapplication.bases.BaseActivity
+
+class MainActivity : BaseActivity() {
+    override val contentView: Int
+        get() = R.layout.activity_main
+
+}
