d56fd4c6200235fd1d1aca0c13f2356b6d85645f
beigirad <f.beigirad@gmail.com>
2019-03-02 00:08:14 +0330

    show VenueListFragment

--- app/src/main/java/ir/beigirad/nearly/MainActivity.kt
+++ app/src/main/java/ir/beigirad/nearly/MainActivity.kt
@@ -1,9 +1,32 @@
 package ir.beigirad.nearly
 
+import androidx.fragment.app.Fragment
+import ir.beigirad.nearly.feature.venuelist.VenueListFragment
 import ir.beigirad.zeroapplication.bases.BaseActivity
 
 class MainActivity : BaseActivity() {
     override val contentView: Int
         get() = R.layout.activity_main
 
+    override fun initUI() {
+        super.initUI()
+        showFragment(VenueListFragment.newInstance())
+    }
+
+
+    private fun showFragment(fragment: Fragment) {
+        if (supportFragmentManager.findFragmentByTag(fragment.javaClass.simpleName) == null)
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.main_container, fragment, fragment.javaClass.simpleName)
+                .addToBackStack(fragment.javaClass.simpleName)
+                .commit()
+    }
+
+    override fun onBackPressed() {
+        if (supportFragmentManager.backStackEntryCount > 1)
+            supportFragmentManager.popBackStack()
+        else
+            super.onBackPressed()
+    }
 }
