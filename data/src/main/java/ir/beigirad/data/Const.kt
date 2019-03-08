a41d1ff00e5988e2f4260855930087e78488c549
beigirad <f.beigirad@gmail.com>
2019-03-08 23:21:21 +0330

    remove default values

--- data/src/main/java/ir/beigirad/data/Const.kt
+++ data/src/main/java/ir/beigirad/data/Const.kt
@@ -4,8 +4,8 @@ package ir.beigirad.data
  * Created by Farhad Beigirad on 3/3/19.
  */
 internal object Const {
-    const val gpsInterval = 0L      //ms
-    const val gpsMinDistance = 0F   //meter
+    const val gpsInterval = 10_000L      //ms
+    const val gpsMinDistance = 100F   //meter
 
     const val minDistance = 100F    //meter
 
